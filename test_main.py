e37bfb3ba66f184fdf97c602368c0d76977be94b
anzedp22 <anze_dp@hotmail.com>
2024-07-25 11:47:25 +0200

    adding new endpoint for app version

--- test_main.py
+++ test_main.py
@@ -5,6 +5,13 @@ from .main import CTS
 client = TestClient(CTS)
 
 
+# Function to read version from version.txt
+def read_version():
+    with open("version.txt", "r") as file:
+        version = file.read().strip()
+    return version
+
+
 # Test helloo world path
 def test_hello_world():
     response = client.get("/")
@@ -16,3 +23,11 @@ def test_hello_world():
 def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
+
+
+# Test version path
+def test_version():
+    expected_version = read_version()
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert response.json() == {"version": expected_version}
